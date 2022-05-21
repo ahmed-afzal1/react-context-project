86d86571df42d1968740372da78e92319f176898
ahmed-afzal1 <afzalbhairab4@gmail.com>
2022-05-21 16:18:04 +0600

    first commit

---
+++ src/components/BookList.js
@@ -0,0 +1,22 @@
+import React, { useContext } from "react";
+import { BookContext } from "../contexts/BookContext";
+// import {read} from 'fs';
+import BookDetails from "./BookDetails";
+
+const BookList = () => {
+    const {books} = useContext(BookContext);
+    return books.length ? ( 
+        <div className="book-list">
+            {
+                books.map(book=>{
+                    return (<BookDetails book={book} key={book.id}/>)
+                })
+            }
+        </div>
+     ) :
+     (
+         <div className="empty"> No books to read. Hello free time :)</div>
+     );
+}
+ 
+export default BookList;
